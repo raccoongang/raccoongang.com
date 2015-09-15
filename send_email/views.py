1536f012e663a67d95a6817b084b24aeaa570424
Ilya <vZ@vZs-MBP-13.lan>
2015-09-15 15:56:38 +0300

    I’m an idiot .. fixed

--- send_email/views.py
+++ send_email/views.py
@@ -45,6 +45,7 @@ def send_email(request):
 
                 msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
                 msg.attach_alternative(html_content, "text/html")
+                msg.send()
 
             except:
                 notification = _('Message has not been sent')
