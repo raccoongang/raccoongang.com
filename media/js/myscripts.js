0cdbe34919b876e8e906a9634fe08e6fa89a2bdb
Ilya <vZ@vZs-MBP-13.lan>
2015-09-17 16:58:28 +0300

    design

--- media/js/myscripts.js
+++ media/js/myscripts.js
@@ -12,9 +12,9 @@ $('#send_form_submit').on('click', function (){
                 success: function(data){
                     console.dir(data);
                     if (data['success']){
-                        $('#mail').val('');
-                        $('#name').val('');
-                        $('#message').val('');
+                        $('#mail').css("border","1px solid black");
+                        $('#name').css("border","1px solid black");
+                        $('#message').css("border","1px solid black");
                         $('#finalMessage').text(data['notification']);
                         $("#success").show().fadeOut(5000);
     //                    $("#success").hide();
@@ -26,8 +26,8 @@ $('#send_form_submit').on('click', function (){
                         $('#name').css("border","3px solid "+data['errors']['name']);
                         $('#mail').css("border","3px solid "+data['errors']['mail']);
                         $('#message').css("border","3px solid "+data['errors']['message']);
-                        $('#finalMessage').text(data['notification']);
-                        $("#success").show().fadeOut(5000);
+//                        $('#finalMessage').text(data['notification']);
+//                        $("#success").show().fadeOut(5000);
                         }
                 }
 	        })
