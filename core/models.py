8cd12b085079db43d72a983623e179de2b4cc919
Ilya <vZ@vZs-MBP-13.lan>
2015-09-18 12:30:54 +0300

    d

--- core/models.py
+++ core/models.py
@@ -4,7 +4,9 @@ from django.db import models
 # Create your models here.
 class Block(models.Model):
     title = models.CharField(max_length=200)
-
+    subtitle = models.CharField(max_length=300,null=True, blank=True)
+    button_title = models.CharField(max_length=100)
+    button_link = models.CharField(max_length=100)
 
     def __unicode__(self):
         return u'Title: %s' % self.title
