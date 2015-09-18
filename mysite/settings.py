c033dbe7d5fc5a80f043603d54ac0436ed323a0e
Ilya <vZ@vZs-MBP-13.lan>
2015-09-18 15:08:39 +0300

    d

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,9 +15,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-print (BASE_DIR)
-print(DATA_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -195,7 +192,8 @@ CMS_TEMPLATES = (
     ('feature.html', 'Page with Feature'),
     ('header.html', 'Header'),
     ('home_page.html', 'Home page'),
-    ('contact_us.html', 'Contact us')
+    ('contact_us.html', 'Contact us'),
+    ('feature_bread.html','Page with Feature (breadcrumbs)')
 )
 
 CMS_PERMISSION = True
