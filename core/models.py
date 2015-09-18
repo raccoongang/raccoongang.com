105cbbc2f10468dd0c2b30c26d56200c1a5b7100
Ilya <vZ@vZs-MBP-13.lan>
2015-09-18 12:19:04 +0300

    CTA unrequired field

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Block(models.Model):
     title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=300)
+    subtitle = models.CharField(max_length=300,null=True, blank=True)
     button_title = models.CharField(max_length=100)
     button_link = models.CharField(max_length=100)
 
