3f6ece7908c8ef83786897f08d9363c8f4adecb8
xahgmah <xahgmah@yandex.ru>
2015-09-26 00:48:32 +0300

    form step gallery

--- questionary/migrations/0001_initial.py
+++ questionary/migrations/0001_initial.py
@@ -15,6 +15,18 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='FormGallery',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=64)),
+                ('image', models.ImageField(upload_to=b'')),
+                ('order', models.PositiveIntegerField(default=0, editable=False, db_index=True)),
+            ],
+            options={
+                'ordering': ['order'],
+            },
+        ),
+        migrations.CreateModel(
             name='FormStep',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -35,5 +47,10 @@ class Migration(migrations.Migration):
                 ('is_draft', models.BooleanField(default=True)),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.AddField(
+            model_name='formgallery',
+            name='formstep',
+            field=models.ForeignKey(to='questionary.FormStep'),
         ),
     ]
