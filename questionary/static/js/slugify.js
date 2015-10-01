ee2aa4c9c3ccb9775d016e208d694eb32c5bdc3b
xahgmah <xahgmah@yandex.ru>
2015-10-01 18:28:12 +0300

    slugify for attribute's slug

--- questionary/static/js/slugify.js
+++ questionary/static/js/slugify.js
@@ -1,22 +1,23 @@
-(function($) {
+django.jQuery(document).ready(function(){
+    django.jQuery('.dynamic-attribute_set .field-slug input').each(function(i){
+        initSlugify(django.jQuery(this).attr('id'));
+    });
+
+});
+
+function initSlugify(slug_id){
     var field;
-
-
     field = {
-        id: '#id_attribute_set-0-slug',
+        id: '#'+slug_id,
         dependency_ids: [],
         dependency_list: [],
         maxLength: 50
     };
 
-    
-    field['dependency_ids'].push('#id_attribute_set-0-name');
+    $('.empty-form .form-row .field-slug, .empty-form.form-row .field-slug').addClass('prepopulated_field');
+    field['dependency_ids'].push('#'+slug_id.replace('-slug','-name'));
     field['dependency_list'].push('name');
-    
 
-    console.log(field);
-    $('.form-row.field-slug').addClass('prepopulated_field');
-    $(field.id).data('dependency_list', field['dependency_list'])
+    django.jQuery(field.id).data('dependency_list', field['dependency_list'])
                .prepopulate(field['dependency_ids'], field.maxLength);
-
-})(django.jQuery);
+}
