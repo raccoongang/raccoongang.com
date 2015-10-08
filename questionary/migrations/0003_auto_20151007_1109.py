bb589e69a23d8ca8bd0ff9bb934ae2eacaa11184
xahgmah <xahgmah@yandex.ru>
2015-10-08 10:18:22 +0300

    add filefield and imagefield

---
+++ questionary/migrations/0003_auto_20151007_1109.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('questionary', '0002_auto_20151006_1109.py'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='edxproject',
+            name='expire_date',
+            field=models.DateField(default=datetime.datetime(2015, 10, 6, 11, 9, 20, 224164), blank=True),
+        ),
+    ]
+
