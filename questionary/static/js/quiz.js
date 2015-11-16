50bcccd0d279e552d3cb809a2b36c928f5e0c48f
Alex Strelets <alex.strelets@gmail.com>
2015-11-16 13:25:38 +0200

    new styles and templates for quiz

--- questionary/static/js/quiz.js
+++ questionary/static/js/quiz.js
@@ -1,9 +1,34 @@
 (function($) {
     var HL_BLOCKS_SELECTOR = '.highlight-block',
-        HL_CONTAINER_SELECTOR = '.highlights';
+        HL_CONTAINER_SELECTOR = '.highlights',
+        HL_AREA_SELECTOR = '.highlight-area';
 
     $(document).ready(function() {
         $highlights = $(HL_CONTAINER_SELECTOR);
+        $areas = $(HL_AREA_SELECTOR);
+        $areas.hide();
+
+        setTimeout(function() {
+            var topOffset = $highlights.offset().top;
+            $areas.each(function() {
+                var $area = $(this),
+                    link = $area.data('linked-to');
+                if (link) {
+                    var $field = $('#id_' + link);
+                    console.log($area.offset().top - topOffset);
+                    console.log($field, $field.offset().top - topOffset);
+                    $area
+                    .css({
+                        position: 'absolute',
+                        left: 0,
+                        right: 30,
+                        top: $field.offset().top - topOffset
+                    })
+                    .show();
+                }
+            })
+        }, 1000);
+
 
         $(HL_BLOCKS_SELECTOR).each(function() {
             var $el = $(this);
