2502511eb879ab8044b75ff5abf3946c58f329b6
xahgmah <xahgmah@yandex.ru>
2015-11-17 12:09:17 +0200

    url bugfix

--- questionary/urls.py
+++ questionary/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from questionary.views import survey_view
 
 urlpatterns = [
-    url(r'^/$', survey_view, name='questionary'),
-    url(r'^/(?P<step>\d+)/$', survey_view, name='questionary'),
+    url(r'^(?P<step>\d+)/$', survey_view, name='questionary'),
     url(r'^', survey_view),
 ]
+
