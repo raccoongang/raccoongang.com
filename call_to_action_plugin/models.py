93acf0625b5c22ed1da71ba9991867fd762f3f18
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2015-12-14 17:59:09 +0200

    Added google analytics to survey button on home_page

--- call_to_action_plugin/models.py
+++ call_to_action_plugin/models.py
@@ -5,6 +5,7 @@ from core.models import Block
 
 class CallToActionPlugin(CMSPlugin):
     block_to_show = models.ForeignKey(Block)
+    google_event = models.CharField(max_length=100, blank=True)
 
     def __unicode__(self):
         return self.block_to_show.title
