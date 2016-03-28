3d3a85048109a8e4379d111bf4b598706323e41b
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-03-28 18:17:57 +0300

    Enlarge name field of email

--- send_email/forms.py
+++ send_email/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class MailForm(forms.Form):
-    name = forms.CharField(max_length=30)
+    name = forms.CharField(max_length=50)
     mail = forms.EmailField(max_length=255)
     message = forms.CharField(max_length=5000)
