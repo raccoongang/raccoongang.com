be7fe2ae37b21438cf60a0771d083f56da3a2826
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-03-28 14:06:40 +0300

    Improve css

--- questionary/cms_app.py
+++ questionary/cms_app.py
@@ -4,7 +4,6 @@ from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
 
-
 class QuestionaryApp(CMSApp):
     name = _('Questionary')
     urls = ['questionary.urls']
